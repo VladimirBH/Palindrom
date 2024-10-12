104c4a5e577f190b53595b3e825549ad443e2037
Vladimir Burbakh <vladimirburbah07@gmail.com>
2024-10-12 09:00:41 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿// string str = Console.ReadLine();
+// bool isPalindrom = true;
+// for(int first = 0, last = str.Length - 1; first < last; first++, last--)
+// {
+//     if(str[first] != str[last])
+//     {
+//         isPalindrom = false;
+//     }
+// }
+// if(isPalindrom)
+// {
+//     Console.WriteLine("Слово является палиндромом");
+// }
+// else
+// {
+//     Console.WriteLine("Слово не является палиндромом");
+// }
+
+// Console.WriteLine("Введите количество элементов массива: ");
+// int count = Convert.ToInt32(Console.ReadLine());
+// int[] intArr = new int[count];
+
+// for (int i = 0; i < count; i++)
+// {
+//     intArr[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// Console.WriteLine("Введите число, по которому идет проверка: ");
+// int chisl = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Количество элементов массива, значения которых больше, указанного вами: {countArrElements(intArr, chisl)}");
+
+// int countArrElements(int[] arr, int chislo)
+// {
+//     count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if(arr[i] >= chislo)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+int count = 5;
+int[,] matrix = new int[count,count];
+
+Random random= new();
+for(int i = 0; i < count; i++)
+{
+    for(int j = 0; j < count; j++)
+    {
+        matrix[i,j] = random.Next(1, 100);
+    }
+    Console.WriteLine();
+}
+
+PrintMatrix(matrix);
+
+for(int i = 0; i < count; i++)
+{
+    for(int j = i; j < count; j++)
+    {
+        int temp = matrix[i,j];
+        matrix[i,j] = matrix[j,i];
+        matrix[j,i] = temp;
+    }
+}
+
+PrintMatrix(matrix);
+
+void PrintMatrix(int[,] mat)
+{
+    for(int i = 0; i < count; i++)
+    {
+        for(int j = 0; j < count; j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
